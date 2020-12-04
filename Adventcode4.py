652a10932e528350baed1abfec63d153326006cf
Kev <kevin.heldner@epfl.ch>
2020-12-04 22:34:42 +0100

    Working line adder

--- Adventcode4.py
+++ Adventcode4.py
@@ -1,9 +1,45 @@
-passports = []
+WOclass passports():
+    byr = "Birth"
+    iyr = "Issue"
+    eyr = "Expiration"
+    hgt = "Height"
+    hcl = "Hair"
+    ecl = "Eye"
+    pid = "Passport"
+    cid = "Country"
+    
+    def __init__(self,byr,iyr,eyr,hgt,hcl,ecl,pid,cid):
+        self.byr = byr
+        self.iyr = iyr
+        self.eyr = eyr
+        self.hgt = hgt
+        self.hcl = hcl
+        self.ecl = ecl
+        self.pid = pid
+        self.cid = cid     
 
-tempblocks = []
+def make_passports ( byr,iyr,eyr,hgt,hcl,ecl,pid,cid ):
+    person = passports(byr,iyr,eyr,hgt,hcl,ecl,pid,cid)
+    return person
+
+temps = []
+
+passports = []
 
 with open('passports.txt', 'r') as fh:
     for line in fh:
-        tempblocks.append(line)
-    if not line.strip()
-        passports.append()
+        temps.append(line.strip())
+
+my_string =""        
+for line in temps:
+    if len(line)>1:
+        my_string += line
+    else:
+        passports.append(my_string)
+        my_string=""
+    
+
+
+for post in passports:
+    print(post)
+print(passports)       
