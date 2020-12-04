373842764b93fed8b214709ce0e99c9040267d97
Kev <kevin.heldner@epfl.ch>
2020-12-04 01:23:05 +0100

    Working stage 1

--- Adventcode3.py
+++ Adventcode3.py
@@ -1,25 +1,27 @@
 pos_x = 0
 pos_y = 0
+right = 3
+downwards = 1
 
 crash = 0
 
 with open('Trees.txt', 'r') as fh:
     for line in fh:
         line = line[:-1]
-        print(pos_x,pos_y%2, len(line), line)
+        print(pos_x,pos_y, len(line), line)
 
         if pos_x < len(line):
-            if line[pos_x] == "#" and pos_y % 2 == 0:
+            if line[pos_x] == "#" and pos_y % downwards == 0:
                 crash += 1
-            pos_x += 7
+            pos_x += right
 
         else:
             pos_x = pos_x-len(line)
             print("repetition", crash)
             print(pos_x,pos_y, len(line), line)
-            if line[pos_x] == "#" and pos_y % 2 == 0:
+            if line[pos_x] == "#" and pos_y % downwards == 0:
                 crash += 1
-            pos_x += 7
+            pos_x += right
 
         pos_y += 1
 print(crash)
