8802f40e95fee5858bb921a73b8bbd20d51fc74a
Kev <kevin.heldner@epfl.ch>
2020-12-04 13:51:24 +0100

    Starting day 4 with if not line.strip

---
+++ Adventcode4.py
@@ -0,0 +1,9 @@
+passports = []
+
+tempblocks = []
+
+with open('passports.txt', 'r') as fh:
+    for line in fh:
+        tempblocks.append(line)
+    if not line.strip()
+        passports.append()
