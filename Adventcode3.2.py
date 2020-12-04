09d9f8c604a57ac01071e8dfb5688a46748c48ca
Kev <kevin.heldner@epfl.ch>
2020-12-04 02:27:31 +0100

    Working one alice

--- Adventcode3.2.py
+++ Adventcode3.2.py
@@ -2,7 +2,7 @@ tab = [(1,1),(3,1),(5,1),(7,1),(1,2)]
 forest = []
 res = 1
 
-with open('day3.txt', 'r') as fh:
+with open('Trees.txt', 'r') as fh:
     for line in fh:
         forest.append(line.strip('\n'))
 
@@ -10,10 +10,14 @@ for (tab_x, tab_y) in  tab:
     x = 0
     y = 0
     tree = 0
-    while y < len(forest) and x < len(forest[0]):
-        if forest[y][x]=="#":
-            tree+=1
+    while y < len(forest) :
+        while x < len(forest[0]):
+            if forest[y][x]=="#":
+                tree+=1
             x+= tab_x
             y+= tab_y
-            x -= len(forest[0])
+            if y >= len(forest):
+                break
+        x -= len(forest[0])
+    res = res*tree
 print(res)
